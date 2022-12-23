557d8835f255a35829c062a68a36d847a8b29376
shroukZain <119010900+shroukZain@users.noreply.github.com>
2022-12-23 13:31:40 +0200

    Update mycode.asm

--- mycode.asm
+++ mycode.asm
@@ -1,3 +1,4 @@
+ ;* This code contains two types of chipers :
  ;*                                           1- MonoNumeric 
  ;*                                           2-MonoAlphaptic             
 
